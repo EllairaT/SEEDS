51e172d78b243eaedd3cd3ccae7107c7c512888c
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-28 01:19:54 +1200

    recreated client app

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-import express from 'express'
+const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+// Connect Database
+connectDB()
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
