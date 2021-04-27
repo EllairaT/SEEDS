5cfa3a2e233f1e52c6921eabef5ab4777001c583
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-28 11:49:53 +1200

    fixing issue with heroku-postbuild

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+
+const path = require('path')
 const connectDB = require('./config/db')
 
 const app = express()
@@ -9,5 +11,14 @@ connectDB()
 app.get('/', (req, res) => res.send('Hello world!'))
 
 const port = process.env.PORT || 8082
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+// ...
+// Right before your app.listen(), add this:
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
