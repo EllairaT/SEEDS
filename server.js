d55944c9161ac55086483d6a8b7c326f2d4c8274
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-28 21:49:26 +1200

    build

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+const path = require('path')
 const connectDB = require('./config/db')
 
 const app = express()
@@ -13,6 +14,9 @@ const port = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 app.listen(port, () => {
