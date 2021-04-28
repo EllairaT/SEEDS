5894861694a23f517dba937038b3b0cea1ae9779
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-28 21:40:02 +1200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB()
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
