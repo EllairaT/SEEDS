fe0cc2bc098471e9ebde52f5664e522648b0b114
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-29 04:54:56 +1200

    Update server.js
    reconnected MongoDB

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express')
 
 const app = express()
 const path = require('path')
+const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 5000
 
+connectDB()
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
