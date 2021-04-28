60a660f5e421dbfae5eb35fb78840dad8360f327
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-29 04:45:10 +1200

    please

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+// server.js
 const express = require('express')
 
-const path = require('path')
-const connectDB = require('./config/db')
-
 const app = express()
+const path = require('path')
 
-const port = process.env.PORT || 5000
-// Connect Database
-connectDB()
+const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello world!'))
+// configure body parser for AJAX requests
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+app.use(express.static('client/build'))
 
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`)
+// routes
+app.get('/', (req, res) => {
+  res.send('Hello from MERN')
+})
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+// Bootstrap server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`)
 })
