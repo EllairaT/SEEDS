5504da10dd7479f3587f8427d197f7d9019c4ebb
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-29 04:13:27 +1200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ connectDB()
 app.get('/', (req, res) => res.send('Hello world!'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('*', express.static(path.join(__dirname, 'client', 'build', 'index.html')))
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 app.listen(port, () => {
